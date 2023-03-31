085a19d6b2ef80a071b5c15d74771ad2cbc34dc6
Alex Ledesma <alex.ledesma7@gmail.com>
2023-03-31 14:52:50 -0400

    adding visibility tag to subnets for filtering in data query within EKS.tf

--- vpc-tf/vpc.tf
+++ vpc-tf/vpc.tf
@@ -23,11 +23,13 @@ module "vpc" {
   public_subnet_tags = {
     "kubernetes.io/cluster/${local.cluster_name}" = "shared"
     "kubernetes.io/role/elb"                      = 1
+    "visibility"                                  = "public" 
   }
 
   private_subnet_tags = {
     "kubernetes.io/cluster/${local.cluster_name}" = "shared"
     "kubernetes.io/role/internal-elb"             = 1
+    "visibility"                                  = "private" 
   }
 }
 
